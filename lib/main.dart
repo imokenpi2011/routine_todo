701f478b8ba05eb8e6488360f3a20b600701a67c
anthem-landing18 <imokenpi2011@gmail.com>
2024-08-14 03:23:57 +0900

    app.dartを切り出し

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:routine_todo/presentations/app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Routine App',
-      theme: ThemeData(),
-      home: const Scaffold(),
-    );
-  }
 }
